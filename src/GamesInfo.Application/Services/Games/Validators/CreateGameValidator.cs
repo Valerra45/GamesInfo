043c2ab7bf0135efe2b234e40ae89fd586e0ca79
Valerra45 <valerra45@gmail.com>
2022-05-30 11:42:27 +0300

    add validation

---
+++ src/GamesInfo.Application/Services/Games/Validators/CreateGameValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using GamesInfo.Application.Services.Games.Commands;
+
+namespace GamesInfo.Application.Services.Games.Validators
+{
+    public class CreateGameValidator : AbstractValidator<CreateGameCommand>
+    {
+        public CreateGameValidator()
+        {
+            RuleFor(p => p.Request.Name)
+               .NotEmpty()
+               .WithMessage("{PropertyName} is not assigned");
+
+            RuleFor(p => p.Request.Description)
+               .NotEmpty()
+               .WithMessage("{PropertyName} is not assigned");
+        }
+    }
+}
---
+++ src/GamesInfo.WebHost/Helpers/ExceptionsHelper.cs
@@ -0,0 +1,15 @@
+﻿using GamesInfo.Core.Exceptions;
+using System.Net;
+
+namespace GamesInfo.WebHost.Helpers
+{
+    public static class ExceptionsHelper
+    {
+        public static Dictionary<Type, HttpStatusCode> ExceptionsHttpStatusCodes
+            => new()
+            {
+                [typeof(ValidationException)] = HttpStatusCode.UnprocessableEntity,
+                [typeof(EntityNotFoundException)] = HttpStatusCode.NotFound
+            };
+    }
+}
